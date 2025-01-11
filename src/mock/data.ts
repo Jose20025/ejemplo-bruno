5e4198172ab06c492d7ef1ac9f74f19fb862f197
José Carlos Gutiérrez <joseca20025@gmail.com>
2025-01-10 20:01:41 -0400

    Refactor data.ts and tsconfig.app.json

--- src/mock/data.ts
+++ src/mock/data.ts
@@ -2,103 +2,103 @@ export const structuresData = [
   {
     name: "Torre Eiffel",
     city: "París",
-    country: "Francia"
+    country: "Francia",
   },
   {
     name: "Arco del Triunfo",
-    city: "París", 
-    country: "Francia"
+    city: "París",
+    country: "Francia",
   },
   {
     name: "Empire State Building",
     city: "Nueva York",
-    country: "Estados Unidos"
+    country: "Estados Unidos",
   },
   {
     name: "Estatua de la Libertad",
     city: "Nueva York",
-    country: "Estados Unidos"
+    country: "Estados Unidos",
   },
   {
     name: "Burj Khalifa",
     city: "Dubái",
-    country: "Emiratos Árabes Unidos"
+    country: "Emiratos Árabes Unidos",
   },
   {
     name: "Torre de Tokio",
-    city: "Tokio", 
-    country: "Japón"
+    city: "Tokio",
+    country: "Japón",
   },
   {
     name: "Templo Senso-ji",
     city: "Tokio",
-    country: "Japón"
+    country: "Japón",
   },
   {
     name: "Torre CN",
     city: "Toronto",
-    country: "Canadá"
+    country: "Canadá",
   },
   {
     name: "Opera House",
     city: "Sídney",
-    country: "Australia"
+    country: "Australia",
   },
   {
     name: "Sagrada Familia",
     city: "Barcelona",
-    country: "España"
+    country: "España",
   },
   {
     name: "Alhambra",
     city: "Granada",
-    country: "España"
+    country: "España",
   },
   {
     name: "Big Ben",
     city: "Londres",
-    country: "Reino Unido"
+    country: "Reino Unido",
   },
   {
     name: "Torre de Londres",
     city: "Londres",
-    country: "Reino Unido"
+    country: "Reino Unido",
   },
   {
     name: "Torre de Pisa",
     city: "Pisa",
-    country: "Italia"
+    country: "Italia",
   },
   {
     name: "Coliseo Romano",
     city: "Roma",
-    country: "Italia"
+    country: "Italia",
   },
   {
     name: "Cristo Redentor",
     city: "Río de Janeiro",
-    country: "Brasil"
+    country: "Brasil",
   },
   {
     name: "Petronas Towers",
     city: "Kuala Lumpur",
-    country: "Malasia"
+    country: "Malasia",
   },
   {
     name: "Torre Taipei 101",
     city: "Taipéi",
-    country: "Taiwán"
+    country: "Taiwán",
   },
   {
     name: "Torre Ostankino",
     city: "Moscú",
-    country: "Rusia"
+    country: "Rusia",
   },
   {
     name: "Catedral de San Basilio",
     city: "Moscú",
-    country: "Rusia"
-  }
+    country: "Rusia",
+  },
 ];
 
 export interface CountryStructureCount {
@@ -107,13 +107,26 @@ export interface CountryStructureCount {
 }
 
 export const getStructuresPerCountry = (): CountryStructureCount[] => {
-  const countByCountry = structuresData.reduce((acc, structure) => {
-    acc[structure.country] = (acc[structure.country] || 0) + 1;
-    return acc;
-  }, {} as Record<string, number>);
+  //! Usando reduce
+  // const countByCountry = structuresData.reduce((acc, structure) => {
+  //   acc[structure.country] = (acc[structure.country] || 0) + 1;
+  //   return acc;
+  // }, {} as Record<string, number>);
 
-  return Object.entries(countByCountry).map(([country, count]) => ({
-    country,
-    count
-  }));
+  // return Object.entries(countByCountry).map(([country, count]) => ({
+  //   country,
+  //   count
+  // }));
+
+  //! Usando groupBy
+  const data = Object.groupBy(structuresData, (structure) => structure.country);
+
+  const normalizedData: CountryStructureCount[] = Object.entries(data).map(
+    ([country, structures]) => ({
+      country,
+      count: structures?.length || 0,
+    }),
+  );
+
+  return normalizedData;
 };
